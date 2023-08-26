0c59fb05301b44859c11bed4c0a4d645a527c3e7
Andrew Song <comsi02@Andrews-Laptop.local>
2023-08-25 22:04:17 -0400

    - add unit test code, but it is not complete.

--- src/test/java/com/omdb/movie/MovieApplicationTests.java
+++ src/test/java/com/omdb/movie/MovieApplicationTests.java
@@ -1,13 +1,36 @@
 package com.omdb.movie;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+@AutoConfigureMockMvc
 @SpringBootTest
 class MovieApplicationTests {
 
+	@Autowired
+	private MockMvc mvc;
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void displayApiFailTest() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("http://www.omdbapi.com/?s=Home&apikey=apikey")).andDo(print())
+				.andExpect(status().is4xxClientError());
+	}
+
+	@Test
+	public void displayApiSuccTest() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("http://www.omdbapi.com/?s=Home&apikey=c5ef16e2")).andDo(print())
+				.andExpect(status().isOk());
 	}
 
 }
