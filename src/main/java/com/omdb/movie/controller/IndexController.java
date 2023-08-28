ba8a005601908d6eaa995d54e5236f63ba90ba41
Andrew Song <comsi02@Andrews-Laptop.local>
2023-08-28 15:38:38 -0400

    - add 4xx and 5xx error page  - fix : placeholder

--- src/main/java/com/omdb/movie/controller/IndexController.java
+++ src/main/java/com/omdb/movie/controller/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
-  @RequestMapping(value = "/")
+  @RequestMapping(value = {"/", "/movie", "/movie/", "/movie/detail", "/movie/detail/"})
   public String index() {
     return "redirect:/movie/list";
   }
